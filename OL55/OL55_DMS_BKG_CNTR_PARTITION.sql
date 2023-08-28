7e05ef2d377d2c3602901feddb6ab2cec3fdebdc
PhuongTXD <phuong.txd@joonsolutions.com>
2023-08-28 17:54:20 +0700

    add ol55_dms_bkg_cntr_partition

---
+++ OL55/OL55_DMS_BKG_CNTR_PARTITION.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE TABLE one-global-dde-uat.bq_log_sink.OL55_DMS_BKG_CNTR_PARTITION
+
+PARTITION BY DATE(EDW_UPD_DT) --replaced from MONTH, changed to DATE
+CLUSTER BY --identical clustering fields as original
+    BKG_NO,
+    CNTR_TPSZ_CD,
+    CNTR_NO
+AS (
+    SELECT
+        *
+    FROM
+        `one-global-dde-uat.DEV_DWH.DMS_BKG_CNTR`
+    WHERE
+        DATE(EDW_UPD_DT) between '2022-08-01' and '2023-08-01'
+)
